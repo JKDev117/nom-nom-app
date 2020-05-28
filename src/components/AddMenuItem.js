2c250c123d6fb908362a96f498d769850aa474ec
Jae Kim <jckim81@yahoo.com>
2020-05-28 14:30:33 -0400

    added static features to AddMenuItem.js & EditMenuItem.js

--- src/components/AddMenuItem.js
+++ src/components/AddMenuItem.js
@@ -6,7 +6,33 @@ class AddMenuItem extends React.Component {
         return(
             <div className="AddMenuItem">
                 <h1>Add New Menu Item</h1>
-                <section>Fill out Form</section>
+                <section>
+                    <form>
+                        <label for="name"> Name: </label>
+                        <input type="text" id="name" name="name"/><br/>
+
+                        <label for="calories"> Calories: </label>
+                        <input type="text" id="calories" name="calories"/><br/>
+
+                        <label for="carbs"> Carbs(g): </label>
+                        <input type="text" id="carbs" name="carbs"/><br/>
+
+                        <label for="protein"> Protein(g): </label>
+                        <input type="text" id="protein" name="protein"/><br/>
+
+                        <label for="fat"> Fat(g): </label>
+                        <input type="text" id="fat" name="fat"/><br/>
+
+                        <label for="category">Category: </label>
+                            <select id="category" name="category">
+                                <option value="Breakfast">Breakfast</option>
+                                <option value="Lunch" selected>Lunch</option>
+                                <option value="Dinner">Dinner</option>
+                            </select><br/>
+                        <button>Add to Menu</button>
+                        <button>Cancel</button>
+                    </form>
+                </section>
             </div>
         )
     }
--- src/components/EditMenuItem.js
+++ src/components/EditMenuItem.js
@@ -6,7 +6,33 @@ class EditMenuItem extends React.Component {
         return(
             <div className="EditMenuItem">
                 <h1>Edit Menu Item</h1>
-                <section>Fill out Form</section>
+                <section>
+                    <form>
+                        <label for="name"> Name: </label>
+                        <input type="text" id="name" name="name"/><br/>
+
+                        <label for="calories"> Calories: </label>
+                        <input type="text" id="calories" name="calories"/><br/>
+
+                        <label for="carbs"> Carbs(g): </label>
+                        <input type="text" id="carbs" name="carbs"/><br/>
+
+                        <label for="protein"> Protein(g): </label>
+                        <input type="text" id="protein" name="protein"/><br/>
+
+                        <label for="fat"> Fat(g): </label>
+                        <input type="text" id="fat" name="fat"/><br/>
+
+                        <label for="category">Category: </label>
+                            <select id="category" name="category">
+                                <option value="Breakfast">Breakfast</option>
+                                <option value="Lunch" selected>Lunch</option>
+                                <option value="Dinner">Dinner</option>
+                            </select><br/>
+                        <button>Update</button>
+                        <button>Cancel</button>
+                    </form>
+                </section>
             </div>
         )
     }
