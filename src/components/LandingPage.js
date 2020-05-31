758bd1f4b5f12db76f92c49822e9e63471de301c
Jae Kim <jckim81@yahoo.com>
2020-05-31 16:30:31 -0400

    added some styling to LandingPage & MenuListPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,8 +8,10 @@ class LandingPage extends React.Component {
             <div className="LandingPage">
                 <h1>My Nom Nom Menu & Meal Planner</h1>
                 <p>This app lets you create your own menu & plan what you want to eat today.</p>
-                <Link to='/menu'>Go To My Menu</Link><br/>
-                <Link to='/meal-plan'>Go To Today's Meal Plan</Link>
+                <section className="landing-page-links">
+                    <Link to='/menu'>Go To My Menu</Link><br/>
+                    <Link to='/meal-plan'>Go To Today's Meal Plan</Link>
+                </section>
             </div>
         )
     }
