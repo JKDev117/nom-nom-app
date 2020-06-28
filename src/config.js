8e2a3392decd14376f978d867e4a9e7c0d754a62
Jae Kim <jckim81@yahoo.com>
2020-06-28 07:38:50 -0400

    modified config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    REACT_APP_API_BASE_URL: 'http://localhost:8000' || process.env.REACT_APP_API_BASE_URL,
+    REACT_APP_API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'nomnom-client-auth-token',
 }
