a03d3254b2f617246200c0bd153e64e2acb1abae
Jae Kim <jckim81@yahoo.com>
2020-08-12 02:06:07 -0400

    added styling to NotFoundPage.js

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import './NotFoundPage.css';
 
 export default class NotFoundPage extends Component {
   render() {
     return (
       <div className='NotFoundPage'>
-        <h2>Page Not Found</h2>
-        <p>This page doesn't exist, try going back or using the navigation menu</p>
+        <h2 className="not-found-title">Page Not Found</h2>
+        <p className="not-found-message">This page doesn't exist, try going back or using the navigation menu</p>
       </div>
     )
   }
